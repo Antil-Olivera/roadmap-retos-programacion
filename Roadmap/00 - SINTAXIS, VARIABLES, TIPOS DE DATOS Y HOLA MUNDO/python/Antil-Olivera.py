292f28dedb117470b3a4eb83d9cf3df8904f4c86
Antil-Olivera <antil.olivera@mi.unc.edu.ar>
2025-02-23 11:45:04 -0300

    Antil-Olivera.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Antil-Olivera.py
@@ -0,0 +1,18 @@
+# https://www.python.org/
+""" https://www.python.org/ 
+https://www.python.org/ 
+https://www.python.org/
+"""
+'''
+https://www.python.org
+'''
+variable = 5
+MY_CONSTANT = "C" 
+
+my_int = 1
+my_float = 2.5
+my_bool = True
+my_bool = False 
+my_str = "Texto"
+
+print("Hola, Python")
